aa6baf22bf9ad5b53941e19260daabcc964cc918
Ryan Chalmers <ryan.chalmers8@gmail.com>
2018-10-02 15:20:13 -0400

    question 1 and 2 complete

---
+++ c/output/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/input.c.o"
+  "CMakeFiles/program.dir/main.c.o"
+  "CMakeFiles/program.dir/job.c.o"
+  "program.pdb"
+  "program"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
